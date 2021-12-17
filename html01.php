418ba15c349e52b7e6c8213fc4e793b8c8a95758
Suttiratsu <63160266@go.buu.ac.th>
2021-12-17 19:57:18 +0700

    add homework file1

---
+++ html01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>html01</title>
+</head>
+<body>
+	line1 <br>
+	line2 <br>
+	line3
+<h1 align = "center" >Heading 1</h1>
+<h2 align = "center">Heading 2</h2>
+<h3 align = "center">Heading 3</h3>
+<h4 align = "center">Heading 4</h4>
+<h5 align = "center">Heading 5</h5>
+<h6 align = "center">Heading 6</h6>
+
+</body>
+</html>
--- welcome_message.php
+++ welcome_message.php
@@ -1,14 +1,144 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Heading 1 to 6</title>
+<title>Show Header</title>
 </head>
 <body>
-<h1>Heading 1</h1>
-<h2>Heading 2</h2>
-<h3>Heading 3</h3>
-<h4>Heading 4</h4>
-<h5>Heading 5</h5>
-<h6>Heading 6</h6>
+	line1 <br>
+	line2 <br>
+	line3
+<h1 align = "center" >Heading 1</h1>
+<h2 align = "center">Heading 2</h2>
+<h3 align = "center">Heading 3</h3>
+<h4 align = "center">Heading 4</h4>
+<h5 align = "center">Heading 5</h5>
+<h6 align = "center">Heading 6</h6>
+
+This is a paragraph.
+This is a paragraph.
+<p>This is a paragraph.</p>
+<p>This is a paragraph.</p>
+
+To force
+line breaks<br>
+in a text,<br>
+use the br<br>
+element.
+<br>
+This is normal text
+<b> and this is bold text </b>
+<br>
+This is some
+<u> mispeled </u>
+<br>
+This is some
+<i> italic </i>
+<br>
+This is
+<sub> subscripted </sub> text
+<br>
+This is
+<sup> superscripted </sup> text
+<br>
+This is normal font <br>
+<font face="Tahoma" size = "14">
+This is Tahoma font
+</font>
+<br>
+<font color="red">Color Names</font>
+<font color="rgb(255,99,71)">RGB values</font>
+<font color="#ff6347">HEX values</font>
+<font color="hsl(9,100%,64%)">HSL values</font>
+<font color="rgba(255,99,71,0.5)">RGBA values</font>
+<font color="hsla(8,100%,64%,0.5)">HSLA values</font>
+<br>
+<h1>The Main section</h1>
+<hr>
+HTML is the standard markup language
+<hr size="5" color="red" />
+HTML is the standard markup language
+<br>
+Non-breaking Space <br>
+Non-breaking Space <br>
+Non-breaking &nbsp; Space <br> 
+Non-breaking &emsp; Space <br> 
+
+<ol type="1">
+<li> Pfizer vaccine
+<li> Moderna vaccine
+</ol>
+<ol type="i">
+<li> Pfizer vaccine
+<li> Moderna vaccine
+</ol>
+<ol type="A">
+<li> Pfizer vaccine
+<li> Moderna vaccine
+</ol>
+
+<ul type="Disc">
+<li> Pfizer vaccine
+<li> Moderna vaccine
+</ul>
+<ul type="Circle">
+<li> Pfizer vaccine
+<li> Moderna vaccine
+</ul>
+<ul type="Square">
+<li> Pfizer vaccine
+<li> Moderna vaccine
+</ul>
+<br>
+<img src="logo-buu.png" >
+<img src="https://www.informatics.buu.ac.th/2020/wp-content/uploads/2018/11/buu_logo_thai-300x300.jpg"/>
+<br>
+<a href=#a> Link 1 </a> <br>
+<a href=#b> Link 2 </a> <br>
+<hr>
+<a name=a> </a> Jump for link 1 <br>
+<a name=b> </a> Jump for link 2 <br>
+
+This example uses a full URL to link to a web page:
+<a href="https://www.w3schools.com">HTML tutorial</a>
+<br>
+<table border="1px">
+<tr>
+	<th>Id</th>
+	<th>Name</th>
+	<th>Score</th>
+	<th>Age</th>
+</tr>
+<tr>
+	<td>55160022</td>
+	<td>Apisit</td>
+	<td>82</td>
+	<td>2</td>
+</tr>
+<tr>
+		<td>1</td>
+		<td>1</td>
+		<td>1</td>
+		<td>22</td>
+</tr>
+</table>
+<br>
+<table border="1px" >
+<tr >
+<td>Name</td>
+<td colspan="2"><center>Apisit</center></td>
+</tr>
+<tr>
+<td>Email</td>
+<td >apisit.sa@buu.ac.th</td>
+<td >apisit.sa@go.buu.ac.th</td>
+</tr>
+<tr>
+<td>Image</td>
+<td colspan="2">
+<img src="logo-buu.png">
+</td>
+</tr>
+<br>
+
 </body>
 </html>
